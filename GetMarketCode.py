63b41ef9d3b32f5daf89085d46bb43a25cd1e3aa
SeongHo5356 <kjeng7897@gmail.com>
2020-12-10 18:39:48 +0900

    BitCoin_trading_bot

--- GetMarketCode.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://api.upbit.com/v1/market/all"
-
-querystring = {"isDetails":"false"}
-
-response = requests.request("GET", url, params=querystring)
-
-print(response.text)
